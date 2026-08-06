91dfaa990f3745ede83f2a4a57c98b524ffb7bde
dip0001-boop <dip0001@tarneitcollege.vic.edu.au>
2026-08-06 11:28:23 +1000

    Update uv.sw.js

--- public/uv/uv.sw.js
+++ public/uv/uv.sw.js
@@ -1,1 +1,11 @@
+importScripts('https://unpkg.com/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.bundle.js');
+importScripts('/uv/uv.config.js');
+importScripts('https://unpkg.com/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.sw.js');
 
+const uv = new UVServiceWorker();
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    uv.fetch(event)
+  );
+});
