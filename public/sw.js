24652a5250ebcc5c5b23f78f612ff447d2d64a3b
dip0001-boop <dip0001@tarneitcollege.vic.edu.au>
2026-08-06 16:19:17 +1000

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -5,9 +5,11 @@ importScripts(__uv$config.sw);
 const uv = new UVServiceWorker();
 
 self.addEventListener('fetch', (event) => {
+    // If the request is trying to use the proxy, route it through Ultraviolet
     if (event.request.url.startsWith(location.origin + __uv$config.prefix)) {
         event.respondWith(uv.fetch(event));
     } else {
+        // Otherwise, just load the normal site files
         event.respondWith(
             caches.match(event.request).then((cached) => cached || fetch(event.request))
         );
